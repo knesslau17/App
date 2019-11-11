3368e101ed442439efdae4baabeb6dbc65494057
p3t3r-k <49058063+p3t3r-k@users.noreply.github.com>
2019-11-11 14:18:05 +0100

    added activities changed to RelativeLayout

---
+++ app/src/main/java/com/example/observations/BlutzuckerActivity.java
@@ -0,0 +1,13 @@
+package com.example.observations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlutzuckerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bz);
+    }
+}
