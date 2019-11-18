8ae9844398ba45775d3d56a82dfffbffd5bb1356
p3t3r-k <49058063+p3t3r-k@users.noreply.github.com>
2019-11-18 17:08:36 +0100

    main activity UI and Intents

--- app/src/main/java/com/example/observations/MainActivity.java
+++ app/src/main/java/com/example/observations/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.observations;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void onClickSwitchToRR(View view) {
+        startActivity(new Intent(MainActivity.this, BlutdruckActivity.class));
+    }
+    public void onClickSwitchToBMI(View view) {
+        startActivity(new Intent(MainActivity.this, BmiActivity.class));
+    }
+    public void onClickSwitchToBZ(View view) {
+        startActivity(new Intent(MainActivity.this, BlutzuckerActivity.class));
+    }
+
+
 }
