4100c9290cbe5b1051caf0ebe49afe9cc8082f02
viktoriaruckhofer <57627832+viktoriaruckhofer@users.noreply.github.com>
2019-11-30 16:40:11 +0100

    Dropdown Liste

--- app/src/main/java/com/example/observations/MainActivity.java
+++ app/src/main/java/com/example/observations/MainActivity.java
@@ -4,14 +4,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String sSelected=parent.getItemAtPosition(position).toString();
+        Toast.makeText(this,sSelected,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.patientList);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Patientennamen, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void onClickSwitchToRR(View view) {
@@ -28,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, BlutzuckerActivity.class));
         MainActivity.this.finish();
     }
+
+
 
 
 }
