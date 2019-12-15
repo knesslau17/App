dc11c1ed54474d40e76a01a4b5741b368c075e78
p3t3r-k <49058063+p3t3r-k@users.noreply.github.com>
2019-12-15 13:31:06 +0100

    updates

--- app/src/main/java/com/example/observations/MainActivity.java
+++ app/src/main/java/com/example/observations/MainActivity.java
@@ -2,27 +2,31 @@ package com.example.observations;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.NetworkOnMainThreadException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
-import android.widget.AdapterView;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String object;
-    Spinner spinner = findViewById(R.id.patientList);
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        object = getPatient.getJSON("http://",10);
+        try {
+            object = getPatient.getNames("http://mirth.grieshofer.com:80/fhir/", 3000);
+        } catch (NetworkOnMainThreadException e) {
+            Toast.makeText(this, "Verbindung fehlgeschalgen", Toast.LENGTH_LONG);
+        }
 
+        spinner = findViewById(R.id.patientList);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Patientennamen, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String sSelected = parent.getItemAtPosition(position).toString();
-        Toast.makeText(this, sSelected, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, sSelected, Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/example/observations/getPatient.java
+++ app/src/main/java/com/example/observations/getPatient.java
@@ -4,20 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class getPatient {
+class getPatient {
 
-    public static String getJSON(String url, int timeout) {
+    static String getNames(String url, int timeout) {
         HttpURLConnection c = null;
         try {
             URL u = new URL(url);
             c = (HttpURLConnection) u.openConnection();
             c.setRequestMethod("GET");
-            c.setRequestProperty("Content-length", "0");
+            c.setRequestProperty("names", "10");
             c.setUseCaches(false);
             c.setAllowUserInteraction(false);
             c.setConnectTimeout(timeout);
@@ -32,14 +31,12 @@ public class getPatient {
                     StringBuilder sb = new StringBuilder();
                     String line;
                     while ((line = br.readLine()) != null) {
-                        sb.append(line + "\n");
+                        sb.append(line).append("\n");
                     }
                     br.close();
                     return sb.toString();
             }
 
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(getPatient.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(getPatient.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
